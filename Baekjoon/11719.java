2f3b7e0fdf76625e56e0c5a0600fd15c4e893c2f
2ssue <e2ssue@gmail.com>
2019-04-08 17:41:16 +0900

    #11719: 그대로 출력하기2

---
+++ Baekjoon/11719.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); 
+        String print;
+        
+        while(scan.hasNextLine()){
+            print = scan.nextLine(); 
+            System.out.println(print);
+        }
+	}
+}
