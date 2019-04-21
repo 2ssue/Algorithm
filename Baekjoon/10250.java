f2629554c21e3b40f698ad98e199e654daf0b144
2ssue <42017052+2ssue@users.noreply.github.com>
2019-04-21 21:30:50 +0900

    #10250: ACM 호텔

---
+++ Baekjoon/10250.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int nTestCase = sc.nextInt();
+		
+		for(int i = 0; i < nTestCase; i++) {
+			int nRow = sc.nextInt();
+			int nFloor = 0;
+			int nWindow = 1;
+			
+			int nCustomer = sc.nextInt();
+			
+			if(nCustomer > nRow) {
+				nWindow += nCustomer/nRow;
+				if(nCustomer % nRow == 0) nWindow--;
+				nCustomer %= nRow;
+			}
+			
+			for(int j = 0; j < nCustomer; j++) {
+				nFloor++;
+			}
+			
+			if(nCustomer == 0) nFloor += nRow;
+			
+			nFloor *= 100;
+			System.out.println(nFloor + nWindow);
+		}
+	}
+}
