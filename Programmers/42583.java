74d18bb4ef35ef090fa6e66a721f2437d6926e1f
2ssue <42017052+2ssue@users.noreply.github.com>
2019-06-11 14:54:57 +0900

    #42583: 다리를 지나는 트럭_2차 코드

--- Programmers/42583.java
+++ Programmers/42583.java
@@ -2,47 +2,42 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 class Solution {
-    public int sum(int[] arr, int idx1, int idx2){
-        int sum = 0;
+    public void increaseTime(Queue<Integer> time) {
+        int queueSize = time.size();
         
-        for(int i = idx1; i <= idx2; i++){
-            sum += arr[i];    
+        for(int i = 0; i < queueSize; i++) {
+            time.offer(time.poll() + 1);
         }
-        
-        return sum;
     }
     
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        int second = 0;
-        
-        Queue que = new LinkedList();
-        int[] delay = new int[truck_weights.length];
-        int front = 0;
-        int rear = 0;
-        
-        que.offer(truck_weights[0]);
-        second++;
-        delay[0]++;
-        
-        while((!que.isEmpty())){
-            second++;
-            for(int i = front; i <= rear; i++){
-                delay[i]++;
-                if(delay[i] > bridge_length){
-                    que.poll();
-                    front++;
-                }
+         
+        int bridge_weight = 0;
+        Queue<Integer> truck_time = new LinkedList<>();
+        int idx = 0;
+         
+        while(idx < truck_weights.length) {
+            answer++;
+             
+            int curTruck = truck_weights[idx];
+             
+            if(bridge_weight + curTruck <= weight) {
+                idx++;
+                bridge_weight += curTruck;
+                truck_time.offer(0);
             }
-            if (!(rear + 1 == truck_weights.length)){
-                if((sum(truck_weights, front, rear) + truck_weights[rear + 1]) <= weight){
-                    que.offer(truck_weights[++rear]);
-                    delay[rear]++;
-                }
+             
+            increaseTime(truck_time);
+             
+            if(truck_time.peek() == bridge_length) {
+                bridge_weight -= truck_weights[idx - truck_time.size()];
+                truck_time.poll();
             }
         }
-        answer = second;
-        
+         
+        answer += bridge_length;
+         
         return answer;
     }
 }
