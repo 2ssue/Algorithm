3b4598dd125d92ba80100ab337a3bf4af90eed6f
2ssue <e2ssue@gmail.com>
2020-04-13 20:49:59 +0900

    JS #42579: 베스트앨범

---
+++ Programmers/42579.js
@@ -0,0 +1,37 @@
+function solution(genres, plays) {
+  const summaryGenres = genres.reduce((acc, cur, index) => {
+    if (acc[`${cur}`] === undefined) {
+      acc[`${cur}`] = {
+        totalPlay: 0,
+        playList: [],
+      };
+    }
+
+    acc[`${cur}`].totalPlay += plays[index];
+    acc[`${cur}`].playList.push([index, plays[index]]);
+
+    return acc;
+  }, {});
+
+  const summaryGenresList = Object.values(summaryGenres).sort(
+    (genre1, genre2) => {
+      return genre2.totalPlay - genre1.totalPlay;
+    },
+  );
+
+  const answer = summaryGenresList.reduce((acc, genre) => {
+    genre.playList.sort((play1, play2) => {
+      return play2[1] - play1[1];
+    });
+
+    acc.push(genre.playList[0][0]);
+
+    if (genre.playList.length > 1) {
+      acc.push(genre.playList[1][0]);
+    }
+
+    return acc;
+  }, []);
+
+  return answer;
+}
